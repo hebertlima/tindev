8a469b604cc3c93dda06893871cd77ef18232a7e
Hebert Lima <hebert.dev@hotmail.com>
2019-08-06 22:12:42 -0300

    server main file

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://root:\@admin@cluster0-3nmv8.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
