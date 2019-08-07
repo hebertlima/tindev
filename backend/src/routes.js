fc4e2c8f810f65df69ac48b813373b249c280928
Hebert Lima <hebert.dev@hotmail.com>
2019-08-06 22:12:27 -0300

    all routes

---
+++ backend/src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const UserController = require('./controllers/UserController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
+
+const routes = express.Router();
+
+routes.get('/', (request, response) => {
+    return response.json({ message: 'tests' });
+});
+
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+routes.post('/users/:id/likes', LikeController.store);
+routes.post('/users/:id/dislikes', DislikeController.store);
+
+module.exports = routes;
